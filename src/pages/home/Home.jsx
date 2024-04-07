05eb0720be385f794564338d22b6c7855d95b031
shivkadiwala20 <shivkadiwala.it20@scet.ac.in>
2024-04-07 21:33:45 +0530

    infinite scroll done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,22 +3,39 @@ import CreatePostModal from '../../components/CreatePostModal';
 import './home.css';
 
 import { useGetPostsQuery } from '../../store/apis/postApi';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react'; // Import useRef
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPost } from '../../actions/action.js';
 
 const Home = () => {
   const [page, setPage] = useState(1);
+
+  const [posts, setPosts] = useState([]);
+  const [isNewPost, setIsNewPost] = useState(false);
+
+  // 5 -> setPosts([...posts, 5]) -> posts = 5
+  // 5 -> setPosts([...posts, 5]) -> posts = 10
+  // 5 -> setPosts([...posts, 5]) -> posts = 15
+  // 1 -> setPosts([...posts, 1]) -> posts = 16 (setHasMore -> false)
+
   const dispatch = useDispatch();
   const { data, isLoading, isSuccess, isError, error } = useGetPostsQuery(page);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const posts = useSelector((state) => state.posts.posts);
+  const initialPosts = useSelector((state) => state.posts.posts);
   const [hasMore, setHasMore] = useState(true);
+  const topRef = useRef(null);
 
   const openModal = () => {
     setIsModalOpen(true);
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Add useRef to keep track of the bottom of the page
+  const bottomOfPage = useRef();
 
   useEffect(() => {
     if (isError) {
@@ -27,31 +44,71 @@ const Home = () => {
   }, [error, isError]);
 
   useEffect(() => {
-    if (isSuccess) {
-      dispatch(fetchPost(data?.data?.data));
-      if (data?.data?.data.length === 0 && page !== 1) {
-        setHasMore(false);
+    if (initialPosts && initialPosts.length > 0) {
+      if (!initialPosts[0].userData) {
+        setIsNewPost(true);
+        return;
+      }
+
+      if (isNewPost) {
+        setPosts([initialPosts[0], ...posts]);
+        setIsNewPost(false);
       } else {
-        setHasMore(true);
+        setPosts([...posts, ...initialPosts]);
       }
     }
-  }, [data, isSuccess]);
+  }, [initialPosts]);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  useEffect(() => {
+    // console.log(posts);
+  }, [posts]);
 
-  const nextPage = () => {
+  useEffect(() => {
+    const newPosts = data?.data?.data;
+    if (newPosts) {
+      console.log(newPosts);
+      setPosts([...posts, ...newPosts]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(fetchPost(data?.data?.data));
+      return;
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data) {
+      const total = data.data?.total;
+      if (page * 5 >= total) {
+        setHasMore(false);
+      }
+    }
+  }, [data, page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const bottomOffset =
+        bottomOfPage.current.getBoundingClientRect().top - window.innerHeight;
+
+      if (bottomOffset < 0) {
+        setPage((prevPage) => prevPage + 1);
+        window.removeEventListener('scroll', handleScroll);
+        if (topRef) {
+          // topRef.current.scrollIntoView({ behavior: 'instant' });
+        }
+      }
+    };
+
     if (hasMore) {
-      setPage(page + 1);
+      window.addEventListener('scroll', handleScroll);
     }
-  };
 
-  const prevPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  };
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [data, hasMore]);
 
   return (
     <>
@@ -66,34 +123,22 @@ const Home = () => {
       <CreatePostModal isOpen={isModalOpen} onClose={closeModal} />
       {isLoading && <p>....Loading</p>}
       {posts?.length === 0 && !isLoading && <p>No posts is there</p>}
-      {posts.map((post) => (
-        <Post
-          key={post?._id}
-          post_id={post.filePath ? post?._id : undefined}
-          username={post?.userData?.username}
-          desc={post?.description}
-          title={post?.title}
-          createdAt={post?.createdAt}
-          post={post}
-        />
-      ))}
-      {isLoading && <p>Loading more posts...</p>}
-      {!isLoading && !hasMore && <p>You're all caught up!</p>}
-      <div className="pagination">
-        {page > 1 && <button onClick={prevPage}>Back</button>}
-        <button
-          onClick={nextPage}
-          style={{
-            display:
-              !hasMore || (posts?.length === 0 && !isLoading)
-                ? 'none'
-                : 'block',
-          }}
-          disabled={!hasMore && posts?.length === 0 && !isLoading}
-        >
-          Next
-        </button>
+      <div className="min-h-screen">
+        {posts.map((post) => (
+          <Post
+            key={post?._id}
+            post_id={post.filePath ? post?._id : undefined}
+            username={post?.userData?.username}
+            desc={post?.description}
+            title={post?.title}
+            createdAt={post?.createdAt}
+            post={post}
+          />
+        ))}
+        {isLoading && <p>Loading more posts...</p>}
+        {!isLoading && !hasMore && <p>You're all caught up!</p>}
       </div>
+      <div ref={bottomOfPage}></div>{' '}
     </>
   );
 };
